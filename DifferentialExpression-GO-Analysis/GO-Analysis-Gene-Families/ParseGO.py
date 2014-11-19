9baea9fcf1e60676b2aeda167ae15445ae76d590
bastodian <bastodian@gmail.com>
2014-11-19 11:16:29 -0500

    GO terms

--- DifferentialExpression-GO-Analysis/GO-Analysis-Gene-Families/ParseGO.py
+++ DifferentialExpression-GO-Analysis/GO-Analysis-Gene-Families/ParseGO.py
@@ -3,6 +3,7 @@
 import sys, re
 
 PTHR_Map = sys.argv[1]
+DE_Results = sys.argv[2]
 
 # Iterate over the PTHR to GO map once and extract all panther
 # family IDs. Every PTHR ID will form a key in a Panther to GO
@@ -24,7 +25,36 @@ with open(PTHR_Map, 'r') as GO_Mapping:
         for Term in GO_Terms:
             GeneFamilyToGO[PTHR_ID].append(Term)
 
-for PTHR_ID, GO_Terms in GeneFamilyToGO.iteritems():
-    GeneFamilyToGO[PTHR_ID] = list(set(GO_Terms))
+#for PTHR_ID, GO_Terms in GeneFamilyToGO.iteritems():
+#    GeneFamilyToGO[PTHR_ID] = list(set(GO_Terms))
+#    for Term in GO_Terms:
+#        sys.stdout.write('%s\t%s\n' % (PTHR_ID, Term))
+
+UpRegulated = {}
+DownRegulated = {}
+with open(DE_Results, 'r') as DE:
+    for Line in DE:
+        try:
+            PTHR_ID = Line.split()[0]
+            # Test if the PTHR ID exists in the Gene Family
+            # to GO dictionary
+            GeneFamilyToGO[PTHR_ID]
+            try:
+                LogFC = float(Line.split()[1])
+                FDR = float(Line.split()[4])
+                if LogFC >= 1 and FDR <= 0.05:
+                    UpRegulated[PTHR_ID] = GeneFamilyToGO[PTHR_ID]
+                elif LogFC <= -1 and FDR <= 0.05:
+                    DownRegulated[PTHR_ID] = GeneFamilyToGO[PTHR_ID]
+            except ValueError:
+                continue
+        except KeyError:
+            continue
+
+for PTHR_ID, GO_Terms in UpRegulated.iteritems():
     for Term in GO_Terms:
-        sys.stdout.write('%s\t%s\n' % (PTHR_ID, Term))
+            sys.stdout.write('%s\t%s\n' % (PTHR_ID, Term))
+
+for PTHR_ID, GO_Terms in DownRegulated.iteritems():
+    for Term in GO_Terms:
+            sys.stderr.write('%s\t%s\n' % (PTHR_ID, Term))
